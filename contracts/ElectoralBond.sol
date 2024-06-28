5c60f05d00f16e88b770297d00e46efd1762871f
Pushkarsingh9900 <pushkarthakur9760@gmail.com>
2024-06-28 12:12:41 +0530

    Adding struct, modifier and func

--- contracts/ElectoralBond.sol
+++ contracts/ElectoralBond.sol
@@ -3,8 +3,26 @@ pragma solidity ^0.8.7;
 
 contract ElectoralBond {
 
-    mapping (string => address) private s_partyToWalletMap;
+    struct User {
+        string name;
+        bool isRegistered;
+    }
 
+    mapping(address => User) private users;
+    mapping (string => address) private s_partyToWalletMap;
+    mapping(address => uint) private userBalances;
+
+    modifier onlyRegisteredUser() {
+        require(users[msg.sender].isRegistered, "User not registered");
+        _;
+    }
+
+    function registerUser(string memory name) public {
+        require(!users[msg.sender].isRegistered, "User already registered");
+        users[msg.sender] = User(name, true);
+        emit UserRegistered(msg.sender, name);
+    }
+    
     function mapAddress (string memory partyName, address waletAddress) public {
         s_partyToWalletMap[partyName] = waletAddress;
     }
