2fa467624b7d47cbf226693851d1febd9afe28fc
Pushkarsingh9900 <pushkarthakur9760@gmail.com>
2024-07-06 23:47:22 +0530

    adding func use funds and getRemarks

--- contracts/ElectoralBond.sol
+++ contracts/ElectoralBond.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.7;
 
 contract ElectoralBond {
-    mapping (string => address) private s_partyToWalletMap;
-    mapping (string => uint) private s_partyBalances;
+    mapping(string => address) private s_partyToWalletMap;
+    mapping(string => uint) private s_partyBalances;
+    mapping(address => string[]) private s_partyRemarks;
+
+    
+    event RemarkLeft(address indexed partyAddress, string partyName, string remark);
 
     function transferBond(string memory partyName, uint amount) public payable {
         address to = s_partyToWalletMap[partyName];
@@ -14,11 +18,11 @@ contract ElectoralBond {
         payable(to).transfer(amount);
     }
 
-    function mapAddress (string memory partyName, address walletAddress) public {
+    function mapAddress(string memory partyName, address walletAddress) public {
         s_partyToWalletMap[partyName] = walletAddress;
     }
 
-    function changeWalletAddress (string memory partyName, address changedWalletAddress) public {
+    function changeWalletAddress(string memory partyName, address changedWalletAddress) public {
         s_partyToWalletMap[partyName] = changedWalletAddress;
     }
 
@@ -28,5 +32,23 @@ contract ElectoralBond {
 
     function getPartyBalance(string memory partyName) public view returns (uint) {
         return s_partyBalances[partyName];
+    }
+
+   
+    function useFunds(string memory partyName, uint amount, string memory remark) public {
+        address partyAddress = s_partyToWalletMap[partyName];
+        require(msg.sender == partyAddress, "Only the registered party can use these funds");
+        require(s_partyBalances[partyName] >= amount, "Insufficient balance");
+
+        s_partyBalances[partyName] -= amount;
+        s_partyRemarks[partyAddress].push(remark);
+
+        
+        emit RemarkLeft(partyAddress, partyName, remark);
+    }
+
+    
+    function getRemarks(address partyAddress) public view returns (string[] memory) {
+        return s_partyRemarks[partyAddress];
     }
 }
